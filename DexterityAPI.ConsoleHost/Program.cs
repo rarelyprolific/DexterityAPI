b91d6d47a77db271c3041f020e4c15758f1f0a10
Alan Gryniewicz <alan.gryniewicz@gmail.com>
2017-03-06 18:46:15 +0000

    Added Data project and ConsoleHost test harness

---
+++ DexterityAPI.ConsoleHost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DexterityAPI.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("************************************************");
+            Console.WriteLine("** Console Test Application for Dexterity API **");
+            Console.WriteLine("************************************************");
+
+            Console.WriteLine("Quick 'n' dirty interface to check database and Entity Framework looks good before implementing the WebAPI");
+        }
+    }
+}
